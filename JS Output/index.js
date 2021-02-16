c1ec6ae0a9eb08dec443bca8d0fdebf7f8da1e48
Hanzla <mhanzla712@gmail.com>
2021-02-16 23:46:56 +0500

    practicesJs

---
+++ JS Output/index.js
@@ -0,0 +1,18 @@
+console.log("this is javascript output practice");
+
+
+//  document.getElementById("add").innerText=1+2;
+
+
+document.write(6+5);
+document.write();
+// window.alert(17+12+2000);
+
+
+// function for print the page in pdf form....!
+function myfunction01()
+{
+    window.print()
+}
+
+
---
+++ JS Where To/index.js
@@ -0,0 +1,6 @@
+console.log('this is try console');
+// this is external javascript file
+function myfunction5()
+{
+    document.getElementById('id') . innerHTML = "this is external javascript";
+}
